8a03a3fa60c914f94d39eb805de8d6d4cdd2399a
Cenk Kılıç <cenk@kilic.dev>
2025-11-24 22:30:48 +0100

    fix: mock

--- test/mocks/services/opnsense.go
+++ test/mocks/services/opnsense.go
@@ -38,6 +38,57 @@ func (_m *MockClientAdapter) EXPECT() *MockClientAdapter_Expecter {
 	return &MockClientAdapter_Expecter{mock: &_m.Mock}
 }
 
+// CheckUnboundService provides a mock function for the type MockClientAdapter
+func (_mock *MockClientAdapter) CheckUnboundService(ctx context.Context) error {
+	ret := _mock.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for CheckUnboundService")
+	}
+
+	var r0 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context) error); ok {
+		r0 = returnFunc(ctx)
+	} else {
+		r0 = ret.Error(0)
+	}
+	return r0
+}
+
+// MockClientAdapter_CheckUnboundService_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'CheckUnboundService'
+type MockClientAdapter_CheckUnboundService_Call struct {
+	*mock.Call
+}
+
+// CheckUnboundService is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *MockClientAdapter_Expecter) CheckUnboundService(ctx interface{}) *MockClientAdapter_CheckUnboundService_Call {
+	return &MockClientAdapter_CheckUnboundService_Call{Call: _e.mock.On("CheckUnboundService", ctx)}
+}
+
+func (_c *MockClientAdapter_CheckUnboundService_Call) Run(run func(ctx context.Context)) *MockClientAdapter_CheckUnboundService_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		run(
+			arg0,
+		)
+	})
+	return _c
+}
+
+func (_c *MockClientAdapter_CheckUnboundService_Call) Return(err error) *MockClientAdapter_CheckUnboundService_Call {
+	_c.Call.Return(err)
+	return _c
+}
+
+func (_c *MockClientAdapter_CheckUnboundService_Call) RunAndReturn(run func(ctx context.Context) error) *MockClientAdapter_CheckUnboundService_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // UnboundCreateHostOverride provides a mock function for the type MockClientAdapter
 func (_mock *MockClientAdapter) UnboundCreateHostOverride(ctx context.Context, req *unbound.HostOverride) (string, error) {
 	ret := _mock.Called(ctx, req)
