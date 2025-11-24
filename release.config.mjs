ed14391f7904b8d447b4eb2dc0847654a1945982
Cenk Kılıç <cenk@kilic.dev>
2025-11-24 23:23:17 +0100

    ci: update publish configuration

---
+++ release.config.mjs
@@ -0,0 +1,32 @@
+export default {
+  branches: [
+    "main",
+    "master",
+    "next",
+    "next-major",
+    {
+      name: "alpha",
+      prerelease: true,
+    },
+    {
+      name: "beta",
+      prerelease: true,
+    },
+    {
+      name: "rc",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "README.md"],
+      },
+    ],
+    ["@semantic-release/github", {}],
+  ],
+};
