9755154e0c9e1985f9539c67641deb33d9c105bd
Labrat <labrat@kilic.dev>
2026-09-25 20:46:37 +0200

    chore(release): finish tags-only releases (K-1781)

--- release.config.mjs
+++ release.config.mjs
@@ -1,4 +1,4 @@
 export default {
   extends: "@cenk1cenk2/semantic-release-config",
-  plugins: [["@cenk1cenk2/semantic-release-config/presets/tag", { provider: [], commit: false }], "@semantic-release/github"],
+  plugins: [["@cenk1cenk2/semantic-release-config/presets/tag", { provider: [], commit: false, changelog: false }], "@semantic-release/github"],
 };
